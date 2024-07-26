b593912d992789339d1e99d326372b0e5e1be29b
Danila <easyezyes@gmail.com>
2024-07-26 08:32:44 +0300

    chore(init react-router): create base router and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import GlobalStyles from './styles/global'
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Users from './pages/Users';
 
 const App = () => {
   return (
-    <div>
-      ok!
-    </div>
+    <>
+      <BrowserRouter>
+          <Route path="/" exact>
+            <Home/>
+          </Route>
+          <Route path="/users">
+            <Users/>
+          </Route>
+      </BrowserRouter>
+
+      <GlobalStyles/>
+    </>
   )
 }
 
